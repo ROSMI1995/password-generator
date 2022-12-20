dbc34b8ddfb0db0712739794f796efdf5083936b
ROSMI1995 <rosmithomas212@gmail.com>
2022-12-20 13:33:48 +0530

    tempaltes added

--- passwordapp/urls.py
+++ passwordapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(''),
+    path('', views.HomeView),
+    path('password/',views.Password_display)
 ]
--- passwordapp/views.py
+++ passwordapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HomeView(request):
+	return render(request, 'index.html')
+
+def Password_display(request):
+	return render(request, 'password_display.html')
+
